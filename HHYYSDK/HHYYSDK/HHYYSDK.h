08fd87ee5c5a8420f7e6498316eb587b3a41c0c6
ren996765767 <996765767@qq.com>
2022-03-09 15:21:12 +0800

    init

---
+++ HHYYSDK/HHYYSDK/HHYYSDK.h
@@ -0,0 +1,18 @@
+//
+//  HHYYSDK.h
+//  HHYYSDK
+//
+//  Created by 易购付 on 2022/3/8.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HHYYSDK.
+FOUNDATION_EXPORT double HHYYSDKVersionNumber;
+
+//! Project version string for HHYYSDK.
+FOUNDATION_EXPORT const unsigned char HHYYSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HHYYSDK/PublicHeader.h>
+
+
---
+++ HHYYSDK/HHYYSDK/HHYYShowName.swift
@@ -0,0 +1,15 @@
+//
+//  HHYYShowName.swift
+//  HHYYSDK
+//
+//  Created by 易购付 on 2022/3/8.
+//
+
+import UIKit
+
+public class HHYYShowName: NSObject {
+  
+    public func showName(){
+        print("我是HH")
+    }
+}
